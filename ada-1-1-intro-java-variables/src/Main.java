90092b1a53252d61e477e55a4c62579b671c542a
Beatriz Dile <bedos-sa@student.42sp.org.br>
2023-10-06 14:23:46 -0300

    variables

---
+++ ada-1-1-intro-java-variables/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Beatriz";
+        System.out.println("Hello, " + name);
+
+        int a;
+        int b =2;
+        a = 3;
+
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplic = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplic);
+        System.out.println(divisao);
+    }
+}
