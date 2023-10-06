04b807b127ee7d5885e122f4465e8b217f356f19
Beatriz Dile <bedos-sa@student.42sp.org.br>
2023-10-06 14:26:51 -0300

    string manipulation

---
+++ ada-1-4-intro-java-string-manipulation/src/Dates.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Dates {
+    public static void main(String[] args) {
+        String nome = "Beatriz";
+        LocalDate hoje = LocalDate.now();
+        Locale brasil = new Locale("pt", "BR");
+        System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        if (agora.getHour() >= 0 && agora.getHour() < 12)
+            saudacao = "bom dia";
+        else if (agora.getHour() >= 12 && agora.getHour() < 18)
+            saudacao = "boa tarde";
+        else if (agora.getHour() >= 18 && agora.getHour() < 24)
+            saudacao = "boa noite";
+        else
+            saudacao = "Olá";
+
+        System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+    }
+}
---
+++ ada-1-4-intro-java-string-manipulation/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Beatriz";
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.length());
+
+        String outroNome = "beatriz";
+        System.out.println(nome.equalsIgnoreCase(outroNome));
+    }
+}
