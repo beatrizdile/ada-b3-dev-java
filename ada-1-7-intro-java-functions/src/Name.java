76d3b942f54897d8fd555e44e7e26013f345e64b
Beatriz Dile <bedos-sa@student.42sp.org.br>
2023-10-06 14:28:30 -0300

    functions

---
+++ ada-1-7-intro-java-functions/src/Name.java
@@ -0,0 +1,10 @@
+public class Name {
+    public static void main(String[] args) {
+        String nome = "Beatriz";
+        saudacao(nome);
+    }
+
+    public static void saudacao(String nome){
+        System.out.println("Hello, " + nome);
+    }
+}
---
+++ ada-1-7-intro-java-functions/src/Soma.java
@@ -0,0 +1,10 @@
+public class Soma {
+    public static void main(String[] args) {
+        int resultado = soma(3, 4);
+        System.out.println(resultado);
+    }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
